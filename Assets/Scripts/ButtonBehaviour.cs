dbe6661f8f85c0138dc9aac315ceb49f1ea418cf
PhilToolan <c17433026@mydit.ie>
2021-06-13 14:35:55 +0100

    Moving Platform

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -7,13 +7,45 @@ public class ButtonBehaviour : MonoBehaviour
 
     public GameObject light;
 
+    [SerializeField] private Transform targetA, targetB; 
+    public GameObject platform;
+    private float speed = 1f; 
+    private bool switching = false;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         //If the Player1 is touching me, print "Collect" in the console
         if (collision.gameObject == Player1.Instance.gameObject)
         {
             light.SetActive(true);
+            if (!switching)
+            {
+                platform.transform.position = Vector2.MoveTowards(platform.transform.position, targetA.position, speed * Time.deltaTime); 
+            }
+            else
+            {
+                platform.transform.position = Vector2.MoveTowards(platform.transform.position, targetB.position, speed * Time.deltaTime);
+            }
+
+            if ( platform.transform.position == targetB.position)
+            {
+                switching = false;
+            }
+            else if ( platform.transform.position == targetA.position)
+            {
+                switching = true;
+            }
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //If the Player1 is touching me, print "Collect" in the console
+        if (collision.gameObject == Player1.Instance.gameObject)
+        {
+            light.SetActive(false);
         }
     }
 }
